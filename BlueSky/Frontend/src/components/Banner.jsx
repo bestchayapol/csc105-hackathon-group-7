455a678145b8cc3d9e1755a78dc5b18eef8cc5f0
ntck <nontakorn.chatk@gmail.com>
2023-05-27 23:32:34 +0700

    Route to pages and format code

--- BlueSky/Frontend/src/components/Banner.jsx
+++ BlueSky/Frontend/src/components/Banner.jsx
@@ -1,35 +1,74 @@
 import { Box, Typography, Button } from "@mui/material";
 import Logo from "../assets/Logo.png";
-
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Banner() {
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
-      <Box sx={{ backgroundColor: "#B0B8CE", height: 100}}>
-        <Box sx={{ display: "flex", flexDirection: "row" }}>
-          <Box
-            sx={{
-              height: 50,
-              width: 50,
-              m: 2,
-              mr: 50,
-              mt:3
-            }}
-            component="img"
-            src={Logo}
-          />
-          <Typography sx={{ color: "white", fontSize: "50px", textAlign: "center",mt:1,ml:7 }}>
-            BlueSky
-          </Typography>
-          <Box sx={{ display: "flex", justifyContent: "flex-end", ml: 45, mt: 2 }}>
-            <Button sx={{ height: 70, width: 70, mr: 3 ,color:'white',fontSize:"15px"}}>
-              SIGNUP
-            </Button>
-            <Button sx={{ height: 70, width: 70,color:"white",fontSize:"15px"}}>
-              LOGIN
-            </Button>
-          </Box>
+    <Box sx={{ backgroundColor: "#B0B8CE", height: 100, width: "100%"}}>
+      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+        <Box
+          sx={{
+            height: 50,
+            width: 50,
+            m: 2,
+            mr: 50,
+            mt: 3,
+            cursor: "pointer",
+          }}
+          component="img"
+          src={Logo}
+          onClick={() => {
+            navigate("/Home");
+          }}
+        />
+        <Typography
+          sx={{
+            color: "white",
+            fontSize: "50px",
+            textAlign: "center",
+            mt: 1,
+            ml: 7,
+          }}
+        >
+          BlueSky
+        </Typography>
+        <Box
+          sx={{ display: "flex", justifyContent: "flex-end", ml: 45, mt: 2 }}
+        >
+          {location.pathname === "/Register" ||
+          location.pathname === "/Login" ? (
+            ""
+          ) : (
+            <>
+              <Button
+                onClick={() => {
+                  navigate("/Register");
+                }}
+                sx={{
+                  height: 70,
+                  width: 70,
+                  mr: 3,
+                  color: "white",
+                  fontSize: "15px",
+                }}
+              >
+                SIGNUP
+              </Button>
+              <Button
+                onClick={() => {
+                  navigate("/Login");
+                }}
+                sx={{ height: 70, width: 70, color: "white", fontSize: "15px" }}
+              >
+                LOGIN
+              </Button>
+            </>
+          )}
         </Box>
       </Box>
+    </Box>
   );
 }
 
